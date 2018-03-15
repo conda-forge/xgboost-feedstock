c12f2bfc52ec1ad9fbdf7e83d9e7751bc93edd37
Ray Donnelly <mingw.android@gmail.com>
2018-03-15 13:18:45 +0000

    Rewrite using conda-build 3 features and AD compilers

---
+++ recipe/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# http://xgboost.readthedocs.io/en/latest/build.html
+if [[ ${OSTYPE} == msys ]]; then
+  if [[ "${ARCH}" == "32" ]]; then
+    # SSE2 is used and we get called from MSVC
+    # CPython so 32-bit GCC needs realignment.
+    export CC="gcc -mstackrealign"
+    export CXX="g++ -mstackrealign"
+  fi
+  cp make/mingw64.mk ./config.mk
+else
+  cp make/config.mk ./config.mk
+fi
+make -j${CPU_COUNT}
