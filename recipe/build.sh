329792e5cd687f4eff820b5ba6bb9a591824274a
John Kirkham <jakirkham@gmail.com>
2019-05-06 15:50:19 -0400

    Build PIC libraries

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,7 @@ fi
 cmake -G "Unix Makefiles" \
       -D CMAKE_BUILD_TYPE:STRING="Release" \
       -D BUILD_SHARED_LIBS:BOOL=ON \
+      -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
       -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
       "${SRC_DIR}"
 make -j${CPU_COUNT}
