51bc52dcff50296ac4a11655d373e869a9c06e31
Matthew R Becker <becker.mr@gmail.com>
2019-08-19 07:57:46 -0500

    grrr

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,11 @@
 
 # http://xgboost.readthedocs.io/en/latest/build.html
 
-if [[ $(uname) == Darwin ]]
-then
-    # this seems to be expected by clang when linking
-    ln -s ${PREFIX}/lib/libomp.dylib ${PREFIX}/lib/libgomp.dylib
-fi
+# if [[ $(uname) == Darwin ]]
+# then
+#     # this seems to be expected by clang when linking
+#     ln -s ${PREFIX}/lib/libomp.dylib ${PREFIX}/lib/libgomp.dylib
+# fi
 
 cmake -G "Unix Makefiles" \
       -D CMAKE_BUILD_TYPE:STRING="Release" \
