a49b37fe7ae14e34567fee666c71bd9175b0f7ef
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-08-20 08:37:22 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,14 +9,16 @@ then
     export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
 fi
 
-cmake -G "Unix Makefiles" \
-      -D CMAKE_BUILD_TYPE:STRING="Release" \
-      -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
-      -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
-      -D VERBOSE=ON \
-      -D CMAKE_VERBOSE_MAKEFILE=ON \
-      --debug-output \
-      "${SRC_DIR}"
-cat $SRC_DIR/CMakeFiles/CMakeOutput.log
-cat $SRC_DIR/CMakeFiles/CMakeError.log
+{
+  cmake \
+    -G "Unix Makefiles" \
+    -D CMAKE_BUILD_TYPE:STRING="Release" \
+    -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
+    -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+    "${SRC_DIR}"
+} || {
+  cat $SRC_DIR/CMakeFiles/CMakeOutput.log
+  cat $SRC_DIR/CMakeFiles/CMakeError.log
+  exit 1
+}
 make -j${CPU_COUNT}
