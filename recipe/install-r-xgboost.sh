2b5ce9fa3823e6e19be507cbb43a5237bac55e30
John Kirkham <jakirkham@gmail.com>
2019-10-28 22:19:37 -0400

    Manually copy over xgboost R bindings
    Now that we are building the R bindings as part of the overall build
    xgboost, change the R install step to just copy the built bindings over
    into the prefix.

--- recipe/install-r-xgboost.sh
+++ recipe/install-r-xgboost.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 if [[ ${OSTYPE} == msys ]]; then
-  if [[ ${r_implementation} == mro-base ]]; then
-    PREFIX=$(cygpath -u ${PREFIX})
-    # TODO :: Shouldn't our Rtools package set this?
-    export BINPREF=${PREFIX}/Rtools/mingw_64/bin/
-  fi
+  # Just for now; should we handle slash fixing in conda-build?
+  PREFIX=${PREFIX//\\//}
+  SRC_DIR=${SRC_DIR//\\//}
 fi
 
-pushd ${SRC_DIR}/R-package
-  # Remove src/Makevars.win because it says:
-  # This file is only used for windows compilation from github
-  # It will be replaced with Makevars.in for the CRAN version
-  # rm src/Makevars.win
-  ${R} CMD INSTALL --build .
-popd
+if [[ ${OSTYPE} == msys ]]; then
+  LIBDIR=${PREFIX}/Library/mingw-w64/lib
+else
+  LIBDIR=${PREFIX}/lib
+fi
+
+mkdir -p ${LIBDIR}/R || true
+cp -Rf ${SRC_DIR}/lib/R ${LIBDIR}/R/
