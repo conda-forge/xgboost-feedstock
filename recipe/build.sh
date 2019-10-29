b23ea40fc1bf648ce55c7006c671b6ae5f7a1435
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-10-28 21:49:10 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,20 @@
     -D CMAKE_BUILD_TYPE:STRING="Release" \
     -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
     -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+    "${SRC_DIR}"
+} || {
+  cat $SRC_DIR/CMakeFiles/CMakeOutput.log
+  cat $SRC_DIR/CMakeFiles/CMakeError.log
+  exit 1
+}
+make -j${CPU_COUNT}
+
+{
+  cmake \
+    -G "Unix Makefiles" \
+    -D CMAKE_BUILD_TYPE:STRING="Release" \
+    -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
+    -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
     -D R_LIB:BOOL=ON \
     "${SRC_DIR}"
 } || {
