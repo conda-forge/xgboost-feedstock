9bbfe9a3c348f015a48a5810d9425a4b139a00f6
Primož Godec <p.godec9@gmail.com>
2021-02-25 13:11:45 +0100

    py-xgboost

--- recipe/install-py-xgboost.sh
+++ recipe/install-py-xgboost.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-. activate "${PREFIX}"
-
 pushd ${SRC_DIR}/python-package
   ${PYTHON} setup.py install --use-system-libxgboost --single-version-externally-managed --record=record.txt
 popd
