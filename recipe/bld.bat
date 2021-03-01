8bcaedc1cd2171c5d11dc305d3f48fb185fb4c04
Uwe L. Korn <uwe.korn@quantco.com>
2021-03-01 16:15:04 +0100

    Build R package in initial build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,11 @@ cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE:STRING="Release" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON ^
+    -DR_LIB=ON -DDR_VERSION=4.0.3 ^
     "%SRC_DIR%"
+if errorlevel 1 exit 1
 
 cmake --build . --target install --config Release
+if errorlevel 1 exit 1
+
 popd
