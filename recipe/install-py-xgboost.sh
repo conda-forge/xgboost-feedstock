e82a47e2e4d6cbaa4539e25593cf94d262f979b5
Uwe L. Korn <uwe.korn@quantco.com>
2021-03-08 20:17:56 +0100

    Skip R 3.6 on arches

--- recipe/install-py-xgboost.sh
+++ recipe/install-py-xgboost.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -exuo pipefail
+
 pushd ${SRC_DIR}/python-package
   ${PYTHON} setup.py install --use-system-libxgboost --single-version-externally-managed --record=record.txt
 popd
