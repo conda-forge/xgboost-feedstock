95e86716179f1893f226bd127b455ee9f05375eb
Uwe L. Korn <uwe.korn@quantco.com>
2021-03-09 08:52:10 +0100

    Switch to ninja for building

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# http://xgboost.readthedocs.io/en/latest/build.html
+set -exuo pipefail
 
-{
-  cmake \
-    -G "Unix Makefiles" \
-    -D CMAKE_BUILD_TYPE:STRING="Release" \
-    -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
-    -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+mkdir -p build-target
+pushd build-target
+  cmake ${CMAKE_ARGS} \
+    -GNinja \
+    -DCMAKE_BUILD_TYPE:STRING="Release" \
+    -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
+    -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
     "${SRC_DIR}"
-} || {
-  cat $SRC_DIR/CMakeFiles/CMakeOutput.log
-  cat $SRC_DIR/CMakeFiles/CMakeError.log
-  exit 1
-}
-make -j${CPU_COUNT}
+  ninja
+popd
