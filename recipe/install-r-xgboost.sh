4f7ca5ca50b5fc23d6f8d51af43d224b67a89232
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-04-29 22:40:28 -0700

    Use CMake to build R package

--- recipe/install-r-xgboost.sh
+++ recipe/install-r-xgboost.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-pushd ${SRC_DIR}/R-package
-  # Remove src/Makevars.win because it says:
-  # This file is only used for windows compilation from github
-  # It will be replaced with Makevars.in for the CRAN version
-  # rm src/Makevars.win
-  ${R} CMD INSTALL --preclean --build . ${R_ARGS}
+mkdir -p ${SRC_DIR}/build-r
+
+pushd ${SRC_DIR}/build-r
+  cmake ${CMAKE_ARGS} \
+      -GNinja \
+      -DCMAKE_BUILD_TYPE:STRING="Release" \
+      -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
+      -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+      -DR_LIB=ON \
+      "${SRC_DIR}"
+  cmake --build . --target install --config Release
 popd
