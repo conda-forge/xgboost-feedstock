d45eb3335bae5cf722af2def92b8bad5860649c7
John Kirkham <jakirkham@gmail.com>
2022-08-31 13:34:45 -0700

    Limit arch builds to 4 threads

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,11 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
     export CMAKE_ARGS="-DUSE_CUDA=ON -DUSE_NCCL=ON -DBUILD_WITH_SHARED_NCCL=ON ${CMAKE_ARGS}"
 fi
 
+# Limit number of threads used to avoid hardware oversubscription
+if [[ "${target_platform}" == "linux-aarch64" ]] || [[ "${target_platform}" == "linux-ppc64le" ]]; then
+    export CMAKE_BUILD_PARALLEL_LEVEL=4
+fi
+
 pushd build-target
 cmake ${CMAKE_ARGS} \
       -GNinja \
