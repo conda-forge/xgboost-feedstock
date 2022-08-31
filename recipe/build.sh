3cbd2d588e3963fc22eb7eb102992f2b2761394f
jakirkham <jakirkham@gmail.com>
2022-08-30 20:28:57 -0700

    Use 2 threads with CUDA arch builds
    CUDA Arch builds die during compilation. So constrain threads a bit
    more.

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,12 @@ fi
 
 # Limit number of threads used to avoid hardware oversubscription
 if [[ "${target_platform}" == "linux-aarch64" ]] || [[ "${target_platform}" == "linux-ppc64le" ]]; then
-    export CMAKE_BUILD_PARALLEL_LEVEL=3
+    # CUDA builds are extra sensitive to oversubscription
+    if [ "${cuda_compiler_version}" != "None" ]; then
+        export CMAKE_BUILD_PARALLEL_LEVEL=2
+    else
+        export CMAKE_BUILD_PARALLEL_LEVEL=3
+    fi
 fi
 
 pushd build-target
