7fa3ef08148af8e8780a6ac3aaef2d15387c56e5
John Kirkham <jakirkham@gmail.com>
2022-08-31 21:58:14 -0700

    Bump number of threads to 6

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ fi
 
 # Limit number of threads used to avoid hardware oversubscription
 if [[ "${target_platform}" == "linux-aarch64" ]] || [[ "${target_platform}" == "linux-ppc64le" ]]; then
-    export CMAKE_BUILD_PARALLEL_LEVEL=4
+    export CMAKE_BUILD_PARALLEL_LEVEL=6
 fi
 
 pushd build-target
