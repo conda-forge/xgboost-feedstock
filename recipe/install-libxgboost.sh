8cd97b9f761124eb87a697c02794a590f5b09c47
jakirkham <jakirkham@gmail.com>
2024-06-28 23:04:01 -0700

    Drop `rabit` install
    This directory doesn't exist in XGBoost 2.1.0.

--- recipe/install-libxgboost.sh
+++ recipe/install-libxgboost.sh
@@ -30,5 +30,4 @@ mkdir -p ${LIBDIR} ${INCDIR}/xgboost ${BINDIR} || true
 cp ${SRC_DIR}/xgboost${EXEEXT} ${BINDIR}/
 cp ${SRC_DIR}/lib/${XGBOOSTDSO} ${SODIR}/
 cp -Rf ${SRC_DIR}/include/xgboost ${INCDIR}/
-cp -Rf ${SRC_DIR}/rabit/include/rabit ${INCDIR}/xgboost/
 cp -f ${SRC_DIR}/src/c_api/*.h ${INCDIR}/xgboost/
