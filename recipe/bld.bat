98d8ba26af235e6bf2ab060df42b98411a14885a
jakirkham <jakirkham@gmail.com>
2024-07-19 21:46:43 -0700

    Lower case `not` in `bld.bat`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
-if NOT "%cuda_compiler_version%"=="None" (
+if not "%cuda_compiler_version%"=="None" (
     set "CMAKE_ARGS=-DUSE_CUDA=ON %CMAKE_ARGS%"
 )
 
