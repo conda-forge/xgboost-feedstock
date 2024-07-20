c91ed93e8e5078870c726d329080cc6017e2381a
jakirkham <jakirkham@gmail.com>
2024-07-20 00:55:07 -0700

    Revert to `PYTHON` env var

--- recipe/install-py-xgboost.sh
+++ recipe/install-py-xgboost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd ${SRC_DIR}/python-package
-  python -m pip install . -vv --config-settings use_system_libxgboost=True
+  ${PYTHON} -m pip install . -vv --config-settings use_system_libxgboost=True
 popd
