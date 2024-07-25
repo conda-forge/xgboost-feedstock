2bc4e87b325bd31f9f1e071159e9d213963cac7e
jakirkham <jakirkham@gmail.com>
2024-07-24 19:08:01 -0700

    Link Microsoft blog on `/Zc:__cplusplus`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,6 +10,7 @@ pushd %SRC_DIR%\build
 :: To make sure `__cplusplus` matches, we pass an additional flag to VC.
 :: Ideally other tooling would do this for us, for now we do it ourselves.
 ::
+:: https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
 :: https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-160
 :: https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
 :: https://github.com/NVIDIA/cccl/blob/82a3ed0282893d6316abde04394f59a9a37a3747/libcudacxx/include/nv/target#L27-L30
