4553232ec3873ea254abe9e14f21a34183a8d032
Hyunsu Cho <phcho@nvidia.com>
2024-07-29 14:38:58 -0700

    Drop 0002 patch

--- recipe/install-r-xgboost.sh
+++ recipe/install-r-xgboost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd ${SRC_DIR}/R-package
-  ${R} CMD INSTALL --preclean --build . ${R_ARGS}
+  ${R} CMD INSTALL --preclean --build . --configure-args="CXXFLAGS=-D_LIBCPP_DISABLE_AVAILABILITY" ${R_ARGS}
 popd
