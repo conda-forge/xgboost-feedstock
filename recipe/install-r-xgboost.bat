5948c86d1a62f9f4bb1dbbfd310129f4dc042ed8
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-04 15:48:09 -0800

    re-enable LIBRARY_PREFIX workaround, disable DLLTOOL workaround

--- recipe/install-r-xgboost.bat
+++ recipe/install-r-xgboost.bat
@@ -2,17 +2,22 @@
 mkdir "%SRC_DIR%"\build-r
 pushd "%SRC_DIR%"\build-r
 
-:: set PATH=%LIBRARY_PREFIX%\bin;%PATH%
+:: set PATH to search for DLLs in %LIBRARY_PREFIX%\bin before %BUILD_PREFIX%\bin
+:: This must be set to avoid a failure in loading the stats package in R. This failure
+:: occurs due to loading some DLLs in %BUILD_PREFIX%\bin which are incompatible with
+:: our r-base distribution.
+set PATH=%LIBRARY_PREFIX%\bin;%PATH%
 
 cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE:STRING="Release" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON ^
-    -DDLLTOOL_EXE=%BUILD_PREFIX%\Library\bin\%DLLTOOL% ^
     -DLIBR_CORE_LIBRARY=%PREFIX%\lib\R\bin\x64\R.lib ^
     -DR_LIB=ON ^
     "%SRC_DIR%"
 if errorlevel 1 exit 1
+
+::    -DDLLTOOL_EXE=%BUILD_PREFIX%\Library\bin\%DLLTOOL% ^
 
 cmake --build . --target install --config Release
 if errorlevel 1 exit 1
