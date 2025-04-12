cac1eff88f5afeed271b0757e4ebb229241a08df
jakirkham <jakirkham@gmail.com>
2025-04-11 17:21:41 -0700

    Use CMake & Ninja to standardize install

--- recipe/install-libxgboost.sh
+++ recipe/install-libxgboost.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
 
+pushd build-target
 if [[ ${OSTYPE} == msys ]]; then
-  # Just for now; should we handle slash fixing in conda-build?
-  PREFIX=${PREFIX//\\//}
-  SRC_DIR=${SRC_DIR//\\//}
-fi
-
-if [[ ${OSTYPE} == msys ]]; then
-  LIBDIR=${PREFIX}/Library/mingw-w64/lib
-  INCDIR=${PREFIX}/Library/mingw-w64/include
-  BINDIR=${PREFIX}/Library/mingw-w64/bin
-  SODIR=${BINDIR}
-  XGBOOSTDSO=xgboost.dll
-  EXEEXT=.exe
+  cmake --install . --config Release  --prefix "${PREFIX}/Library/mingw-w64"
 else
-  LIBDIR=${PREFIX}/lib
-  INCDIR=${PREFIX}/include
-  BINDIR=${PREFIX}/bin
-  SODIR=${LIBDIR}
-  if [[ $(uname) == Darwin ]]; then
-    XGBOOSTDSO=libxgboost.dylib
-  else
-    XGBOOSTDSO=libxgboost.so
-  fi
-  EXEEXT=
+  cmake --install . --config Release  --prefix "${PREFIX}"
 fi
-
-mkdir -p ${LIBDIR} ${INCDIR}/xgboost ${BINDIR} || true
-cp ${SRC_DIR}/lib/${XGBOOSTDSO} ${SODIR}/
-cp -Rf ${SRC_DIR}/include/xgboost ${INCDIR}/
-cp -f ${SRC_DIR}/src/c_api/*.h ${INCDIR}/xgboost/
+popd
