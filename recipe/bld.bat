0ceb4a284f97ca95aea5c4288b3f30ffa1a3500c
jakirkham <jakirkham@gmail.com>
2025-04-18 19:58:54 -0700

    Drop leftover verbose flag in `bld.bat`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -20,7 +20,7 @@ if errorlevel 1 exit 1
       -DR_LIB:BOOL=OFF ^
       "%SRC_DIR%"
   if errorlevel 1 exit 1
-  cmake --build . --config Release -- -v
+  cmake --build . --config Release
   if errorlevel 1 exit 1
 
 popd
