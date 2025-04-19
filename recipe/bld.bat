54b916e593e412498abe44a90ef343f5925e24d5
jakirkham <jakirkham@gmail.com>
2025-04-18 21:03:12 -0700

    Set Windows CMake install prefix to `mingw-w64`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -17,7 +17,7 @@ if errorlevel 1 exit 1
         %CMAKE_ARGS% ^
         %XGB_CMAKE_ARGS% ^
         -DCMAKE_BUILD_TYPE:STRING="Release" ^
-        -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+        -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%\mingw-w64" ^
         -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON ^
         -DR_LIB:BOOL=OFF ^
         "%SRC_DIR%"
