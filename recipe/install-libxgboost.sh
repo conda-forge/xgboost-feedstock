d3cbcf81fdee0b8dab705f66e895519eaad4581d
jakirkham <jakirkham@gmail.com>
2025-04-18 21:12:26 -0700

    Rely on CMake configuration values for install
    As we already configured and built a release build, there should be no
    need to specify that at install time. Also the host environment path was
    already specified in the build. That same is used for all the split
    package installs. So we can just rely on that configuration value when
    installing with CMake.

--- recipe/install-libxgboost.sh
+++ recipe/install-libxgboost.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [[ ${OSTYPE} == msys ]]; then
-  export INSTALL_PREFIX="${PREFIX}/Library/mingw-w64"
-else
-  export INSTALL_PREFIX="${PREFIX}"
-fi
-
 pushd build-target
-  cmake --install . --config Release --prefix "${INSTALL_PREFIX}"
+  cmake --install .
 popd
