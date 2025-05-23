829a7d3cea9aeaadc1debf035e0adbfc9b3919aa
Hyunsu Cho <phcho@nvidia.com>
2025-05-23 19:47:34 -0400

    Use recursive delete for headers

--- recipe/install-libxgboost.sh
+++ recipe/install-libxgboost.sh
@@ -6,8 +6,8 @@ pushd build-target
     cmake --install .
 
     rm -fv "${PREFIX}/lib/libdmlc.a"
-    rm -fv "${PREFIX}/include/dmlc"
+    rm -rfv "${PREFIX}/include/dmlc"
     rm -fv "${LIBRARY_PREFIX}/mingw-w64/lib/dmlc.lib"
-    rm -fv "${LIBRARY_PREFIX}/mingw-w64/include/dmlc"
+    rm -rfv "${LIBRARY_PREFIX}/mingw-w64/include/dmlc"
 
 popd
