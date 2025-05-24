3a82747dbb4fd5d6164e6e571eeaa5655e26b4ac
automatic conda-forge administrator <condaforge@gmail.com>
2025-05-24 08:25:56 +0200

    Drop `dmlc` static library (#237)
    * dummy commit for rerendering
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.19.00.08.06
    
    * Drop `libdmlc.a`
    
    * Remove libdmlc.a in install step
    
    * Bump build number
    
    * Update library path
    
    * Remove headers too
    
    * Use recursive delete for headers
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <121827174+conda-forge-webservices[bot]@users.noreply.github.com>
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>
    Co-authored-by: jakirkham <jakirkham@gmail.com>
    Co-authored-by: Hyunsu Cho <phcho@nvidia.com>

--- recipe/install-libxgboost.sh
+++ recipe/install-libxgboost.sh
@@ -5,4 +5,9 @@ pushd build-target
 
     cmake --install .
 
+    rm -fv "${PREFIX}/lib/libdmlc.a"
+    rm -rfv "${PREFIX}/include/dmlc"
+    rm -fv "${LIBRARY_PREFIX}/mingw-w64/lib/dmlc.lib"
+    rm -rfv "${LIBRARY_PREFIX}/mingw-w64/include/dmlc"
+
 popd
