e2082a760789943bf0bf66fc9122d383406d45ea
jakirkham <jakirkham@users.noreply.github.com>
2025-05-24 23:44:27 -0700

    Remove `dmlc`'s `cmake` directory

--- recipe/install-libxgboost.sh
+++ recipe/install-libxgboost.sh
@@ -7,7 +7,9 @@ pushd build-target
 
     rm -fv "${PREFIX}/lib/libdmlc.a"
     rm -rfv "${PREFIX}/include/dmlc"
+    rm -rfv "${PREFIX}/lib/cmake/dmlc"
     rm -fv "${LIBRARY_PREFIX}/mingw-w64/lib/dmlc.lib"
+    rm -rfv "${LIBRARY_PREFIX}/mingw-w64/lib/cmake/dmlc"
     rm -rfv "${LIBRARY_PREFIX}/mingw-w64/include/dmlc"
 
 popd
