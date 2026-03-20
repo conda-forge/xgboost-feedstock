cc9b9abdab333df26507dd0af4bf9e22c5df9acd
Hyunsu Cho <phcho@nvidia.com>
2026-03-20 13:46:00 -0700

    Exclude sm60

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,6 +6,10 @@ if errorlevel 1 exit 1
 set XGB_CMAKE_ARGS=""
 if not "%cuda_compiler_version%" == "None" (
     set "XGB_CMAKE_ARGS=-DUSE_CUDA:BOOL=ON"
+    if "%cuda_compiler_version%" == "12.9" (
+        rem Disable sm_60 to work around NVIDIA/cccl#7982
+        set "CUDAARCHS=70;75;80;86;89;90;100;103;120;121"
+    )
 )
 
 mkdir build-target
