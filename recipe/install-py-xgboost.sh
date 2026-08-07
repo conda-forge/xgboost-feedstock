2fac66564c9c50321b67ab5eea47635eb9bc6c61
Jiaming Yuan <jm.yuan@outlook.com>
2026-08-07 23:19:42 +0800

    fix.

--- recipe/install-py-xgboost.sh
+++ recipe/install-py-xgboost.sh
@@ -5,6 +5,7 @@ pushd ${SRC_DIR}/python-package
 
     # Remove NCCL dependency
     ${PYTHON} ${SRC_DIR}/ops/script/pypi_variants.py --use-suffix=na --require-nccl-dep=na
-    ${PYTHON} -m pip install . -vv -C=cmake.define.XGBOOST_USE_SYSTEM_LIBXGBOOST=ON -Cwheel.platlib=false
+    ${PYTHON} -m pip install . -vv \
+        --config-settings=cmake.define.XGBOOST_USE_SYSTEM_LIBXGBOOST=ON --config-settings=wheel.platlib=false
 
 popd
