fed2fb7377664535ff1762fdaa938605b8cce4bf
Hyunsu Cho <phcho@nvidia.com>
2026-08-10 14:02:18 -0700

    Address review comment

--- recipe/install-py-xgboost.sh
+++ recipe/install-py-xgboost.sh
@@ -5,7 +5,8 @@ pushd ${SRC_DIR}/python-package
 
     # Remove NCCL dependency
     ${PYTHON} ${SRC_DIR}/ops/script/pypi_variants.py --use-suffix=na --require-nccl-dep=na
-    ${PYTHON} -m pip install . -vv \
-        --config-settings=wheel.platlib=false --config-settings=wheel.cmake=false
+    ${PYTHON} -m pip install . -vvv \
+        --config-settings=wheel.platlib=false \
+        --config-settings=wheel.cmake=false
 
 popd
